e8c1c87769dca6a3a06a091388e1bf0a6af64fbe
Rohil Jain <rohiljain@ucsb.edu>
2026-04-07 23:36:30 -0700

    RJ - Added equals and hash testing

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,28 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
-
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+    @Test
+    public void equals_returns_correct_bool()
+    {
+	Team team2 = new Team("test-team");
+	assertEquals(true, team.equals(team2));
+	team2.addMember("Bob");
+	assertEquals(false, team.equals(team2));
+	Team team3 = new Team("team-test");
+	assertEquals(false, team.equals(team3));
+    }
+    @Test
+    public void hash_returns_correct_value()
+    {
+	int result = team.hashCode();
+	int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
 }
